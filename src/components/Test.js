5937adbe721c91b303f8f3e8b80ca71c5033ab73
Mijail <mijail.iso@ciencias.unam.mx>
2021-04-15 01:23:29 -0500

    first draft

--- src/components/Test.js
+++ src/components/Test.js
@@ -8,8 +8,25 @@ const Screen = styled.div`
   position: fixed;
   z-index: -1;
   border: 20px solid black;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
 `;
-
+const TopHeader = styled.div`
+  width: 100%;
+  height: 140px;
+  background: green;
+  display: flex;
+  justify-content: space-between;
+`;
+const Body = styled.div`
+  width: 100%;
+  height: 704.781px;
+  background: yellow;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 // const Logo = styled.div`
 //   width: 400px;
 //   height: 150px;
@@ -18,17 +35,108 @@ const Screen = styled.div`
 //   margin: 30px;
 // `;
 
+const Container = styled.div`
+  width: 1200px;
+  height: 600px;
+  background-color: red;
+  display: grid;
+  grid-template-columns: repeat(3, 400px);
+  overflow-y: scroll;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
 function Test() {
   const sytledLogo = {
     margin: '40px',
     width: '190px',
-    height: '60px'
+    height: '60px',
+    // position: 'absolute',
+  }
+  const styledEx = {
+    width: '50px',
+    height: '50px',
+    backgroundColor: 'black',
+    borderRadius: '50%',
+    position: 'absolute',
+    right: '20px',
+    bottom: '20px'
+  }
+  const stye = {
+    display: 'flex'
+  }
+  const perfil = {
+    width: '250px',
+    height: '250px',
+    backgroundColor: 'black',
+    borderRadius: '50%',
+  }
+  const perfilBox = {
+    width: '400px',
+    height: '300px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
   return (
     <>
     <Screen>
-      <img src={logo} alt="" style={sytledLogo}></img>
-      {/* <Logo></Logo> */}
+      <TopHeader>
+        <div style={stye}>
+          <img src={logo} alt="" style={sytledLogo}></img>
+          <p>Abarrotes en General</p>
+        </div>
+        <img src={logo} alt="" style={sytledLogo}></img>
+      </TopHeader>
+      <Body>
+        {/* <Logo></Logo> */}
+        <Container>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          <div style={perfilBox}>
+            <div style={perfil}></div>
+          </div>
+          
+          
+          {/* <div style={styledEx}></div> */}
+        </Container>
+        <div style={styledEx}></div>
+      </Body>
     </Screen>
     </>
   );
